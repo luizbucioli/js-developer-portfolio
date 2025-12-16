52d3a3051ed630f07f58c68879416f6a5ead74fa
luizbucioli <luizzzbucioli1010@gmail.com>
2025-12-16 17:38:37 -0300

    interagindo com a API

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,7 @@
 
 
-function fetchProfileData(){
-    
-    fetch
+async function fetchProfileData(){
+    const url = 'https://raw.githubusercontent.com/luizbucioli/js-developer-portfolio/refs/heads/main/data/profile.json'
+    const fetching = await fetch(url)
+    return await fetching.json()
 }
