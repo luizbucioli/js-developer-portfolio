8fe6ac814fadcb2daad7227b03d3421695523d29
luizbucioli <luizzzbucioli1010@gmail.com>
2025-12-18 12:03:08 -0300

    Pegando as informações da API como: idiomas e projetos feitos

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,8 @@
 
 
 async function fetchProfileData(){
-    const url = 'https://raw.githubusercontent.com/luizbucioli/js-developer-portfolio/refs/heads/main/data/profile.json'
-    const fetching = await fetch(url)
-    return await fetching.json()
-}
+    const url = 'https://raw.githubusercontent.com/luizbucioli/js-developer-portfolio/refs/heads/main/data/profile.json';
+    const response = await fetch(url)
+    const profileData = await response.json()
+    return profileData
+};
